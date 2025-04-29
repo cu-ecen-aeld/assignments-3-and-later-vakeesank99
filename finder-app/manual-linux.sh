b1544048b9741163a1519ca720485ea6caf010db
vakeesank99 <vakeesank99@gmail.com>
2025-04-29 21:39:05 +0530

    dependencies corrected

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -40,7 +40,7 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig #config
     # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} menuconfig #config
     make -j 8 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all    #build all
-    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} modules # build any kernel modules
+    #make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} modules # build any kernel modules
     # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} Image  #kernel build    
     make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} dtbs      #build device tree
 
@@ -60,7 +60,7 @@ fi
 
 # TODO: Create necessary base directories
 mkdir ${OUTDIR}/rootfs
-cd rootfs
+cd ${OUTDIR}/rootfs
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
 mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
@@ -73,8 +73,8 @@ then
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
-    make distclean
-    make defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} distclean
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
     # make menuconfig
 
 else
@@ -83,7 +83,7 @@ fi
 
 # TODO: Make and install busybox
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+make CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 cd ${OUTDIR}/rootfs
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
@@ -93,10 +93,10 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 cd "${OUTDIR}/rootfs"
 SYSROOT_PATH=$(${CROSS_COMPILE}gcc --print-sysroot)
 echo ${SYSROOT_PATH}
-cp -a ${SYSROOT_PATH}/lib/ld-linux-aarch64.so.1 lib
-cp -a ${SYSROOT_PATH}/lib64/libm.so.6 lib64
-cp -a ${SYSROOT_PATH}/lib64/libresolv.so.2 lib64
-cp -a ${SYSROOT_PATH}/lib64/libc.so.6 lib64
+cp -a ${SYSROOT_PATH}/lib/ld-linux-aarch64.so.1 ./lib/
+cp -a ${SYSROOT_PATH}/lib64/libm.so.6 ./lib64/
+cp -a ${SYSROOT_PATH}/lib64/libresolv.so.2 ./lib64/
+cp -a ${SYSROOT_PATH}/lib64/libc.so.6 ./lib64/
 
 # TODO: Make device nodes
 cd "${OUTDIR}/rootfs"
@@ -105,8 +105,8 @@ sudo mknod -m 600 dev/console c 5 1
 # sudo mknod dev/ram b 1 0
 # TODO: Clean and build the writer utility
 cd $FINDER_APP_DIR
-make clean
-make CROSS_COMPILE=${CROSS_COMPILE}
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} clean
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
@@ -119,13 +119,10 @@ cp conf/username.txt ${OUTDIR}/rootfs/home/conf
 
 
 # TODO: Chown the root directory
-cd ${OUTDIR}/rootfs
-sudo chown -R root:root *
+sudo chown -R root:root ${OUTDIR}/rootfs
 
 # TODO: Create initramfs.cpio.gz
 cd ${OUTDIR}/rootfs
 find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
 
-cd ${OUTDIR}
-
-gzip -f initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
