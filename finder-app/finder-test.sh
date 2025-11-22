f275910b6637b9ff6da05f95a738a9bc2a7aebc5
vakeesank99 <vakeesank99@gmail.com>
2025-11-22 17:37:04 +0530

    writer added

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -54,7 +54,7 @@ fi
 
 #for i in $( seq 1 $NUMFILES)
 #do
-./writer "$WRITEDIR/assignment4-result.txt" "$WRITESTR"
+writer "$WRITEDIR/assignment4-result.txt" "$WRITESTR"
 #done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
