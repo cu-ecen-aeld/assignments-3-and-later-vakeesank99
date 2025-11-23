a48b7b18319b3d059daf25148d91f3e321dbc9bc
vakeesank99 <vakeesank99@gmail.com>
2025-11-23 13:00:15 +0530

    assignment 4 part 2 modif

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -4,28 +4,27 @@
 #include <stddef.h>
 #include <errno.h>
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        exit(1);
-    }
-    const char *filename = argv[1];
-    printf("%s\n",filename);
+	if (argc < 2) {
+        	return EXIT_FAILURE;
+    	}
+    	const char *filename = argv[1];
+    	printf("%s\n",filename);
 	FILE *file = fopen(filename,"wb");
 	if (file == NULL) {
 		fprintf(stderr, "Value of errorno attempting to open file %s: %d\n", filename, errno);
 		perror("perror returned");
 		openlog(NULL,0,LOG_USER);
 		syslog(LOG_ERR, "Invalid Number of arguments: %d", argc);
-        	exit(1);
+        	return EXIT_FAILURE;
 		//fprintf(stderr, "Error opening file %s: %s\n", filename, strerror(errno));
 
-	} else {
-		fprintf(file, "%s",argv[2]);
-openlog(NULL,0,LOG_USER);
-        	syslog(LOG_DEBUG, "Writing %s to %s", argv[2],argv[1]);
+	}
+	const char *content = argv[2]; 	
+	if (fputs(content, file) < 0) {
+		openlog(NULL,0,LOG_USER);
+        	syslog(LOG_DEBUG, "Writing %s to %s", content, filename);
 	}
 	closelog();
     	fclose(file);
-	// syslog
-
-    	return 0;
+    	return EXIT_SUCCESS;
 }
